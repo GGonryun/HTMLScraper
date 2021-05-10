8cfdedc337f5f3c487f4588adfc08a2fa556bdc0
GGonryun <amodestduck@gmail.com>
2021-05-10 15:39:50 -0700

    [+] scrape websites and output to console

--- api/server.js
+++ api/server.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+const fetch = require('node-fetch');
+const websites = ["https://turkish123.com/", "https://luna0cosmetics.myshopify.com/", "http://all-guitar-chords.com/"];
+
+websites.forEach(
+    website => fetch(website)
+        .then(response => response.text())
+        .then(html => console.log(html))
+        .catch(err => console.log('Failed to fetch page: ', err))
+);
+    
