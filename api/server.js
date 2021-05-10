5c1af2bd7433ac838c84d1bc9e3cb59a29f689d1
GGonryun <amodestduck@gmail.com>
2021-05-10 15:51:17 -0700

    [~] ignore output, encapsulate file creation

--- api/server.js
+++ api/server.js
@@ -1,25 +1,30 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
+
 const FILE_ENCODING = "utf8";
 const DATA_FOLDER = "data";
-const websites = ["https://turkish123.com/", "https://luna0cosmetics.myshopify.com/", "http://all-guitar-chords.com/"];
+const OUTPUT_FILE_EXTENSION = "html";
+const WEBSITES = ["https://turkish123.com/", "https://luna0cosmetics.myshopify.com/", "http://all-guitar-chords.com/"];
 
 function cleanUrl(url) {
     return url.replace(/(https|http)|[/\\?%*:|"<>]/g, '');
 }
 
-websites.forEach(
+function generateFileName(website) {
+    return `./${DATA_FOLDER}/${cleanUrl(website)}.${OUTPUT_FILE_EXTENSION}`;
+}
+
+function generateFile(website, html) {
+    const file = generateFileName(website);
+    fs.writeFile(file, html, FILE_ENCODING, (err) => {
+        if(err) throw err;
+        console.log(`Created File => ${file}`);
+    })
+}
+
+WEBSITES.forEach(
     website => fetch(website)
         .then(response => response.text())
-        .then(html => {
-            const file = `./${DATA_FOLDER}/${cleanUrl(website)}.txt`;
-            fs.writeFile(file, html, FILE_ENCODING, (err) => {
-                if(err) throw err;
-                return `Created File => ${file}`;
-            })
-        })
+        .then(html => generateFile(website, html))
         .catch(err => console.log('Failed to fetch page: ', err))
 );
-    
-// fs.writeFile(filename, data, [encoding], [callback])
-
