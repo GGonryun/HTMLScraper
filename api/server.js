66f2d8874a9a5c68f35e946dadd77d4245fa2f3a
GGonryun <amodestduck@gmail.com>
2021-05-10 19:56:06 -0700

    [~] remove magic values

--- api/server.js
+++ api/server.js
@@ -3,13 +3,16 @@ const express = require('express')
 const cors = require('cors')
 const scraper = require('./scraper');
 
-const app = express()
-const port = process.env.PORT || 5000
+const port = settings.PORT || 5000;
+const STATUS = { ERROR: 400, SUCCESS: 200 };
+const CONTENT_TYPE = { HEADER: 'Content-Type', PLAIN: 'text/plain' };
+
+const app = express();
 
 app.use(cors())
 
 app.get('/heartbeat', (req, res) => {
-    res.status(200).json({success: "ok"});
+    res.status(STATUS.SUCCESS).json({success: "ok"});
 })
 
 app.get('/api/scrape', async (req, res) => {
@@ -17,14 +20,13 @@ app.get('/api/scrape', async (req, res) => {
     if(website) {
         try {
             const result = await scraper.scrape(website);
-            return res.set('Content-Type', 'text/plain').status(200).send(`${result.toString()}`);
+            return res.set(CONTENT_TYPE.HEADER, CONTENT_TYPE.PLAIN).status(200).send(`${result.toString()}`);
         } catch(error) {
-            return res.status(400).send("Please provide a valid url.", error);
+            return res.status(STATUS.ERROR).send(`Please provide a valid url: ${error.message}`);
         }
     }
-    return res.status(400).send("Please provide a website.");
+    return res.status(STATUS.ERROR).send("Please provide a website.");
 })
-
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
